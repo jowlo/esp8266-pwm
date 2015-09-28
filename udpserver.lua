d1d22aeb1600784f810f74253b8691a82f31a053
Jonas Wloka <jowlo@uni-bremen.de>
2015-09-28 14:20:59 +0200

    cleanup

--- udpserver.lua
+++ udpserver.lua
@@ -8,45 +8,46 @@ PWM_CMD = 126 -- 0x7e
 
 -- pins in ascending order of frequency ranges and total of pins.
 pins = { 8, 4, 3, 2, 1 }
-numout = 5
 
 -- minimal duty cycle. zero for default off
-min = 65
+min = 0
 
 -- max duty cycle. above 1023 will err.
 max = 1023
 
 -- afterglow
-glow = true
+glow = false
 reduce = 20
 
 -- gpio.mode(pin,gpio.OUTPUT)
-for i=1,numout do
+for i=1,#pins do
     pwm.setup(pins[i], 100, 0)
     pwm.start(pins[i])
 end
 
+function read2b(bstr)
+    return (string.byte(bstr,1)*256 + string.byte(bstr,2))
+end
+
 srv=net.createServer(net.UDP)
 srv:on("receive", function(srv, pl)
-    cmd = string.byte(pl,2)
-    if(cmd ~= PWM_CMD) then return else
---   print("Command Received")
-    for i=1,numout do 
-        byte = string.byte(pl,2 + i*2)*256 + string.byte(pl,2 + i*2+1)
---   print(byte)
-        if(byte > max-min) 
-             --gpio.write(pin, gpio.HIGH)
+    cmd = read2b(pl)
+    -- print("pwm cmd")
+    if(cmd == PWM_CMD) then
+    for i=1,#pins do 
+        val = read2b(string.sub(pl,1+2*i)) + min
+        if(val > max) 
             then pwm.setduty(pins[i], max)
             else
               if(glow)
                 then
                  before = pwm.getduty(pins[i])
-                 if((byte+min) < before - reduce)
+                 if((val+min) < before - reduce)
                   then pwm.setduty(pins[i], before-reduce)
-                  else pwm.setduty(pins[i], byte+min)
+                  else pwm.setduty(pins[i], val)
                  end
                 else
-                 pwm.setduty(pins[i], byte+min)
+                 pwm.setduty(pins[i], val)
               end
         end
      end
