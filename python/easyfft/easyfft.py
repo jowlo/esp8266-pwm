55218b2880a54f4e714431a522186e4dde4eeb2a
Jonas Wloka <jowlo@uni-bremen.de>
2016-01-04 11:09:45 +0100

    working easyfft

--- python/easyfft/easyfft.py
+++ python/easyfft/easyfft.py
@@ -10,12 +10,14 @@ class FFT:
         self.sample_rate = 44100
         self.no_channels = 1
         self.chunk = 2048 # Use a multiple of 8
+        self.buffer = 4*self.chunk
         self.data_in = aa.PCM(aa.PCM_CAPTURE, aa.PCM_NORMAL, 'front:CARD=Set,DEV=0')
         self.data_in.setchannels(self.no_channels)
         self.data_in.setrate(self.sample_rate)
         self.data_in.setformat(aa.PCM_FORMAT_S16_LE)
         self.data_in.setperiodsize(self.chunk)
         self.matrix = []
+        self.logpower = []
         self.noise = []
         self.run_thread = False
 
@@ -29,9 +31,18 @@ class FFT:
         fourier = np.delete(fourier, len(fourier) - 1)
         # Find amplitude
         power = np.log10(np.abs(fourier)) ** 2
+        power2 = power[:]
         # Araange array into 8 rows for the 8 bars on LED matrix
         power = np.reshape(power, (8, self.chunk / 16))
         matrix = np.int_(np.mean(power, axis=1))
+        # Try logarithmic binning here
+        start = 1
+        self.logpower = []
+        for i in [x**2 for x in range(10)]:
+            start += i
+            self.logpower.append(np.int_(np.mean(power2[start:start+i])))
+        print(self.logpower)
+
         if remove_noise:
             self.matrix = np.clip([ (a-b) for a,b in zip(matrix, self.noise)],0,500)
         else:
