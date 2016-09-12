ca51c4ad3197ca3106aa4e126c3e18ab9f9e8edb
Jonas Wloka <jowlo@uni-bremen.de>
2016-09-12 15:18:28 +0200

    DMX Bridge improvements

--- dmxread/DMXBridge/DMXBridge.ino
+++ dmxread/DMXBridge/DMXBridge.ino
@@ -21,7 +21,9 @@
 #include <WiFiUdp.h>
 #include "config.h"
 
+// Prototypes
 void gotDMXCallback(int slots);
+void send_packet();
 
 
 #define PWM_CMD  126 // 0x7e
@@ -29,7 +31,17 @@ void gotDMXCallback(int slots);
 
 WiFiUDP Udp;
 
+
+const int DMX_LED = 14;
+const int ACTIVE_LED = 16;
+const int ACTIVE_SWITCH = 12;
+const int PACKET_DELAY = 20;
+
 int got_dmx = 0;
+int got_dmx_time = 0;
+int active = 0;
+int last_packet_time = 0;
+
 
 IPAddress broadcast_ip, localip, remoteip;
 
@@ -37,8 +49,16 @@ unsigned int localport, remoteport;
 
 void setup() {
   
+
+  // Setup outputs and inputs
   pinMode(BUILTIN_LED, OUTPUT);
-  pinMode(14, OUTPUT);
+
+  
+  pinMode(DMX_LED, OUTPUT);
+  pinMode(ACTIVE_LED, OUTPUT);
+  pinMode(ACTIVE_SWITCH, INPUT_PULLUP);
+
+
   ESP8266DMX.setDataReceivedCallback(&gotDMXCallback);
   delay(1000);        //avoid boot print??
   ESP8266DMX.startInput();
@@ -59,15 +79,17 @@ void setup() {
   remoteport = 5555;
   remoteip = WiFi.localIP();
   remoteip[3] = 1;
+  // remoteip[3] = 23;
   broadcast_ip = WiFi.localIP();
   broadcast_ip[3] = 255;
 
   /*
+  Serial.begin(115200);
   Serial.println("WiFi connected");
   Serial.println("IP address (localip): ");
   Serial.println(WiFi.localIP());
   Serial.println("IP dst: ");  
-  Serial.println(remote_ip);
+  Serial.println(remoteip);
   */
   delay(1000);
 }
@@ -76,7 +98,8 @@ void setup() {
 // ***************** input callback function *************
 
 void gotDMXCallback(int slots) {
-  got_dmx = slots;
+  got_dmx = true;
+  got_dmx_time = millis();
 }
 
 /************************************************************************
@@ -86,23 +109,34 @@ void gotDMXCallback(int slots) {
   
 *************************************************************************/
 void send_beacon();
+int i = 0;
 void loop() {
-  if ( got_dmx ) {
-    /* DMX */
-    // ESP8266 PWM is 10bit 0-1024
-    int a = ESP8266DMX.getSlot(1);
-    analogWrite(14,2*a);
-    Udp.beginPacket(remoteip, localport);
-    Udp.write((uint8_t)0);
-    Udp.write(PWM_CMD);
-    for(int i = 1; i < 31; i++){
-      uint8_t val = ESP8266DMX.getSlot(i);
-      Udp.write(val>>4);
-      Udp.write(val<<4);
-    }
-    Udp.endPacket();
+  // Read in switch
+  int lastactive = active;
+  active = (!digitalRead(ACTIVE_SWITCH));
+  
+  digitalWrite(ACTIVE_LED, active);
+  
+  if(got_dmx_time > (millis() - 200)){
+    digitalWrite(DMX_LED, HIGH);
+  } else {
+    digitalWrite(DMX_LED, LOW);
   }
-  delay(30);
+
+  if (!active && lastactive){
+    ESP8266DMX.clearSlots();
+    send_packet();
+  }
+
+  
+  if (got_dmx) {
+    got_dmx = 0;
+    // Send DMX over Wifi if active, but not too many packets
+    if (active && last_packet_time < (millis() - PACKET_DELAY)) {
+      last_packet_time = millis();
+      send_packet();
+    }
+  }
 }
 
 void send_beacon() {
@@ -113,6 +147,18 @@ void send_beacon() {
   Udp.write(":");
   Udp.write(localport);
   Udp.write(". No connection.");
+  Udp.endPacket();
+}
+
+void send_packet() {
+  Udp.beginPacket(remoteip, localport);
+  Udp.write((uint8_t)0);
+  Udp.write(PWM_CMD);
+  for(int i = 1; i < 31; i++){
+    uint8_t val = ESP8266DMX.getSlot(i);
+    Udp.write(val>>4);
+    Udp.write(val<<4);
+  }
   Udp.endPacket();
 }
 
